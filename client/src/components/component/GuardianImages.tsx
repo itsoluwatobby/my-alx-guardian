776e05394468b1331f5db538724f496e4869cd44
LOVELY-WANDYTE\personal <itsoluwatobby@gmail.com>
2024-05-30 20:28:13 +0100

    fix(comment-deletio): fixed comment deletion

--- client/src/components/component/GuardianImages.tsx
+++ client/src/components/component/GuardianImages.tsx
@@ -7,12 +7,13 @@ type ImagesProps = {
   alt?: string;
   loading?: 'eager' | 'lazy';
   textSize?: string;
+  isLoading?: boolean;
 }
 
-export default function GuardianImages({ imageUri, classNames, imageClassNames, alt, loading, textSize = 'text-lg' }: ImagesProps) {
+export default function GuardianImages({ imageUri, isLoading, classNames, imageClassNames, alt, loading, textSize = 'text-lg' }: ImagesProps) {
 
   return (
-    <figure className={`${imageUri ? '' : 'flex items-center'} bg-slate-700 ${classNames} border-white`}>
+    <figure className={`${imageUri ? '' : 'flex items-center'} ${isLoading? 'animate-pulse' : 'animate-none'} bg-slate-700 ${classNames} border-white`}>
       {
         imageUri ?
         <img src={imageUri} loading={loading} alt={`${alt}-profile`} className={`w-full h-full object-cover ${imageClassNames}`} />
