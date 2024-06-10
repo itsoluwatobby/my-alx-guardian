09f74c08daf47f471a256b35b043cca7bbdcca07
Oluwatobi Akinola Samuel <itsoluwatobby@gmail.com>
2024-06-10 11:24:11 +0100

    Update config.ts

--- client/src/app/config.ts
+++ client/src/app/config.ts
@@ -2,10 +2,10 @@ import axios from 'axios';
 import { isProd } from '../utility/helpers';
 import localStore from '../utility/localStorage';
 
-// const DEPLOYED_LINK = 'https://my-alx-guardian-service.onrender.com/v1';
-const LOCAL_LINK = 'http://localhost:3500/v1';
+const DEPLOYED_LINK = 'https://my-alx-guardian-service.onrender.com/v1';
+// const LOCAL_LINK = 'http://localhost:3500/v1';
 
-export const BASEURL = isProd ? LOCAL_LINK : LOCAL_LINK;
+export const BASEURL = isProd ? DEPLOYED_LINK : DEPLOYED_LINK;
 
 export const guardianAPI = axios.create({
   baseURL: BASEURL,
